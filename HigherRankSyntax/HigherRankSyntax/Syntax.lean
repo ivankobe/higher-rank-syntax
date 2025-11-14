5536a52dd2f142320f1ada72637dedd3a0eb12cf
ivankobe <ivan.kobe@gmail.com>
2025-11-14 14:51:07 +0100

    unit done

--- HigherRankSyntax/HigherRankSyntax/Syntax.lean
+++ HigherRankSyntax/HigherRankSyntax/Syntax.lean
@@ -1,14 +1,17 @@
-
 import Lean.Level
 import Mathlib.CategoryTheory.Category.Basic
 import Mathlib.CategoryTheory.DiscreteCategory
 import Mathlib.Data.Fintype.Basic
+import Mathlib.Data.Finset.Max
+import Mathlib.Data.Finset.Union
+import Mathlib.Data.Finset.Lattice
+import Mathlib.Data.List.MinMax
+import Mathlib.Data.List.Basic
+import Mathlib.Data.List.Lemmas
 
 open CategoryTheory
 
 set_option autoImplicit false
-
--- universe u
 
 inductive Position where
   | empty : Position
@@ -22,6 +25,11 @@ def Position.toType : Position → Type
 
 instance : CoeSort Position Type where
   coe := Position.toType
+
+def Position.toList : (X : Position) → List X.toType
+  | empty => []
+  | slot => [.unit]
+  | oplus p q => (.map .inl p.toList) ++ (.map .inr q.toList)
 
 inductive Arity : Type where
   | mk : (dom : Position) → (dom → Arity) → Arity
@@ -72,100 +80,86 @@ inductive ExpressionAux : Shape → Type :=
 def Expression : Shape → Arity → Type := fun γ α =>
   ExpressionAux (γ ⊕ α)
 
-def V (γ : Shape) (α : Arity) : Type := { x : γ // γ x = α }
+def V (γ : Shape) (α : Arity) : Type := { x : γ | γ x = α }
 
--- Weird: since when must subtype coercions be made explicit?
 instance {γ : Shape} {α : Arity} : CoeOut (V γ α) γ where
   coe x := x.val
 
+def Arity.subArities (α : Arity) : List Arity :=
+  match α with
+  | Arity.mk X p =>
+    match X with
+    | .empty => []
+    | .slot => [p ()]
+    | .oplus X1 X2 =>
+      let α1 : Arity := Arity.mk X1 (λ x => p (Sum.inl x))
+      let α2 : Arity := Arity.mk X2 (λ x => p (Sum.inr x))
+      α1.subArities ++ α2.subArities
+
+def Arity.sizeOf (α : Arity) : Nat := match α with
+  | .mk X A =>
+    match (List.map (λ x => (A x).sizeOf) X.toList).maximum with
+    | none   => 0
+    | some n => n + 1
+
+lemma ArityTypeListMem (α : Arity) (x : α.dom.toType) : x ∈ α.dom.toList :=
+  match α with
+  | .mk X A =>
+    match X with
+    | .empty => by
+      exfalso
+      exact x.elim
+    | .slot => by apply List.mem_cons_self
+    | .oplus Y Z => match x with
+      | .inl y => by
+        apply List.mem_append_left
+        apply List.mem_map_of_mem
+        let α' : Arity := .mk Y (fun _ => .mk .empty Empty.elim)
+        exact ArityTypeListMem α' y
+      | .inr z => by
+        apply List.mem_append_right
+        apply List.mem_map_of_mem
+        let α' : Arity := .mk Z (fun _ => .mk .empty Empty.elim)
+        exact ArityTypeListMem α' z
+
+lemma SizeSubArityLeAux {n m : Nat} (l : List Nat) (p : n ∈ l) (q : List.maximum l = some m) : n ≤ m :=
+  (List.maximum_eq_coe_iff.1 q).2 n p
+
+lemma SizeSubArityLe (α : Arity) (x : α) : (α x).sizeOf < α.sizeOf :=
+  match α with
+  | .mk X A =>
+    match hmax : (List.map (λ x => (A x).sizeOf) X.toList).maximum with
+    | some n => by calc
+      (A x).sizeOf ≤
+        (List.map (λ x => (A x).sizeOf) X.toList).maximum.getD 0 := by
+        rw [hmax]
+        simp
+        apply SizeSubArityLeAux (List.map (λ x => (A x).sizeOf) X.toList)
+        . simp
+          use x
+          . simp
+            apply ArityTypeListMem
+        . assumption
+      _ = n := by aesop
+      _ < n + 1 := by simp
+      _ = (Arity.mk X A).sizeOf := by unfold Arity.sizeOf ; aesop
+    | none => by
+      have h_empty : (List.map (λ x => (A x).sizeOf) X.toList) = [] :=
+        List.maximum_eq_bot.mp hmax
+      have mem : x ∈ X.toList := ArityTypeListMem (Arity.mk X A) x
+      have mem_mapped : (A x).sizeOf ∈ List.map (λ x => (A x).sizeOf) X.toList := by
+        simp [mem]
+        use x
+      rw [h_empty] at mem_mapped
+      simp at mem_mapped
 
 def var {γ : Shape} {α : Arity} : V γ α → Expression γ α := by
   intro ⟨x,p⟩
   cases p
   exact .app (Sum.inl x) (λ (i : γ x) => var ⟨Sum.inr i, by aesop⟩)
-
-
-  -- Σ' (x : domShape γ) , ((i : domArity (mapShape γ x)) →
-  --   ExpressionAux (γ ⊕ (mapArity (mapShape γ x) i)))
-
-
-
--- inductive Shape where
---   /-- the empty shape -/
--- | empty : Shape
---   /-- the shape containing precisely one item -/
--- | slot : Shape → Shape
---   /-- shape extension -/
--- | oplus : Shape → Shape → Shape
--- deriving Repr
--- -- TODO: implement better Repr instance for shapes
-
--- @[inherit_doc]
--- notation "𝟘" => Shape.empty
-
--- @[inherit_doc]
--- notation (priority := default+1) γ:31 " ⊕ " δ:31 => Shape.oplus γ δ
-
--- /-- A synonym for a shape, used when we think of a shape as specifying
---     the arity of a variable. -/
--- abbrev Arity := Shape
-
--- /-- The rank of a shape is the level of nesting of the slots -/
--- @[reducible]
--- def Shape.rank : Shape → Nat
--- | 𝟘 => 0
--- | .slot γ  => 1 + rank γ
--- | γ ⊕ δ => max (rank γ) (rank δ)
-
--- /-- Variables of given arity in a given shape -/
--- inductive var_in  : Arity → Shape → Type where
--- | varHere : ∀ {α : Shape}, var_in α α.slot
--- | varLeft : ∀ {γ δ} {{α}}, var_in α γ → var_in α (γ ⊕ δ)
--- | varRight : ∀ {γ δ} {{α}}, var_in α δ → var_in α (γ ⊕ δ)
-
--- /-- Fold on all variables in a given shape -/
--- def Shape.fold.{u} (γ : Shape) {A : Type u} (a : A) (f : A → ∀ ⦃α⦄, var_in α γ → A) : A :=
---   match γ with
---   | 𝟘 => a
---   | .slot _ => f a .varHere
---   | γ₁ ⊕ γ₂ => γ₂.fold (γ₁.fold a (fun b _ x => f b x.varLeft)) (fun b _ x => f b x.varRight)
-
--- theorem rank_Var_lt {α γ} (x : var_in α γ) : α.rank < γ.rank := by
---   induction x
---   case varHere => simp
---   case varLeft δ β α _ _ =>
---     simp [Shape.rank]
---     calc
---       α.rank < δ.rank := by assumption
---            _ ≤ max δ.rank β.rank := by exact Nat.le_max_left δ.rank β.rank
---   case varRight δ β α _ _ =>
---     simp [Shape.rank]
---     calc
---       α.rank < β.rank := by assumption
---            _ ≤ max δ.rank β.rank := by exact Nat.le_max_right δ.rank β.rank
-
--- /-- Expressions over a given shape -/
--- inductive Expr : Shape → Arity → Type where
--- /-- Apply a free variable to arguments -/
--- | applyFree  : ∀ {γ δ α}, var_in α γ → (∀ ⦃β⦄, var_in β α → Expr (γ ⊕ δ) β) →  Expr γ δ
--- /-- Apply a bound variable to arguments -/
--- | applyBound : ∀ {γ δ α}, var_in α δ → (∀ ⦃β⦄, var_in β α → Expr (γ ⊕ δ) β) →  Expr γ δ
-
--- @[inherit_doc]
--- infix:80 " ◃ " => Expr.applyFree
-
--- @[inherit_doc]
--- infix:80 " ◂ " => Expr.applyBound
-
--- @[reducible]
--- def Expr.sizeOf {γ δ} : Expr γ δ → Nat
--- | @Expr.applyFree _ _ α _ ts => 1 + α.fold 0 (fun n _ y => n + (ts y).sizeOf)
--- | @Expr.applyBound _ _ α _ ts => 1 + α.fold 0 (fun n _ y => n + (ts y).sizeOf)
-
--- theorem Expr.sizeOfFreeArg {γ δ α} (x : var_in α γ) (ts : ∀ ⦃β⦄, var_in β α → Expr (γ ⊕ δ) β) {θ} (y : var_in θ α) :
---   (ts y).sizeOf < (x ◃ ts).sizeOf := sorry
-
--- theorem Expr.sizeOfBoundArg {γ δ α} (x : var_in α δ) (ts : ∀ ⦃β⦄, var_in β α → Expr (γ ⊕ δ) β) {θ} (y : var_in θ α) :
---   (ts y).sizeOf < (x ◂ ts).sizeOf := sorry
-
--- instance {γ δ} : SizeOf (Expr γ δ) where sizeOf := Expr.sizeOf
+termination_by α.sizeOf
+decreasing_by
+  simp_all
+  calc
+  (((γ ⊕ γ x) (.inl x)) i).sizeOf = (γ x i).sizeOf := by aesop
+  _ < (γ x).sizeOf := SizeSubArityLe (γ x) i
